3b0253dd9e6bba878491de7a7b8a9929f841094c
Ilya Tkachev <skaatt@mail.ru>
2022-06-14 11:13:36 +0300

    Add files via upload

--- neural_machine_translation/modules.py
+++ neural_machine_translation/modules.py
@@ -27,7 +27,7 @@ class Encoder(nn.Module):
     def forward(self, src):
         
         #src = [src sent len, batch size]
-        
+
         # Compute an embedding from the src data and apply dropout to it
         embedded = self.dropout(self.embedding(src))
         
@@ -37,6 +37,9 @@ class Encoder(nn.Module):
         # outputs, hidden and cell should be initialized here. Refer to nn.LSTM docs ;)
         
         outputs, (hidden, cell) = self.rnn(embedded)
+        if self.bidirectional:
+          hidden = hidden.view(1, -1, 2 * self.hid_dim)
+          cell = cell.view(1, -1, 2 * self.hid_dim)
         
         #outputs = [src sent len, batch size, hid dim * n directions]
         #hidden = [n layers * n directions, batch size, hid dim]
@@ -64,13 +67,15 @@ class Attention(nn.Module):
         # hidden = [1, batch size, dec_hid_dim]
         
         # repeat hidden and concatenate it with encoder_outputs
-        '''your code'''
+        # print(encoder_outputs.shape, hidden.shape)
+        expand_hidden = hidden.expand(encoder_outputs.shape[0], *hidden.shape[1:])  # hidden repeats `src sent len` times, other dim are same
+        concat_enc_dec_tensor = torch.cat([expand_hidden, encoder_outputs], dim=-1)
         # calculate energy
-        '''your code'''
+        energy = self.v(self.attn(concat_enc_dec_tensor))
         # get attention, use softmax function which is defined, can change temperature
-        '''your code'''
+        attention_softmax = softmax(energy)
             
-        return '''your code'''
+        return attention_softmax
     
     
 class DecoderWithAttention(nn.Module):
@@ -83,11 +88,11 @@ class DecoderWithAttention(nn.Module):
         self.output_dim = output_dim
         self.attention = attention
         
-        self.embedding = '''your code'''
+        self.embedding = nn.Embedding(output_dim, emb_dim)
         
-        self.rnn = '''your code''' # use GRU
+        self.rnn = nn.GRU(emb_dim + enc_hid_dim, dec_hid_dim) # use GRU
         
-        self.out = '''your code''' # linear layer to get next word
+        self.out = nn.Linear(emb_dim + enc_hid_dim + dec_hid_dim, output_dim) # linear layer to get next word
         
         self.dropout = nn.Dropout(dropout)
         
@@ -97,25 +102,31 @@ class DecoderWithAttention(nn.Module):
         
         #n directions in the decoder will both always be 1, therefore:
         #hidden = [n layers, batch size, hid dim]
-        
+
         input = input.unsqueeze(0) # because only one word, no words sequence 
-        
+
         #input = [1, batch size]
-        
         embedded = self.dropout(self.embedding(input))
         
         #embedded = [1, batch size, emb dim]
         
         # get weighted sum of encoder_outputs
-        '''your code'''
+        attention = self.attention(hidden, encoder_outputs)
+        weighted_sum_encoder_outputs = (attention * encoder_outputs).sum(dim=0).unsqueeze(0)
         # concatenate weighted sum and embedded, break through the GRU
-        '''your code'''
+        # input for gru is [1, batch size, hidden] so cat along dim=2
+        # print(weighted_sum_encoder_outputs.shape, embedded.shape)
+        concat_features = torch.cat([weighted_sum_encoder_outputs, embedded], dim=2) 
+        rnn_output, _ = self.rnn(concat_features, hidden)
         # get predictions
-        '''your code'''
+        # input for linear is [1, batch size, hidden] so cat along dim=2
+        # print(weighted_sum_encoder_outputs.shape, embedded.shape, rnn_output.shape)
+        concat_features = torch.cat([weighted_sum_encoder_outputs, embedded, rnn_output], dim=2)
+        prediction = self.out(concat_features)
         
         #prediction = [batch size, output dim]
         
-        return '''your code'''
+        return prediction, rnn_output
         
 
 class Seq2Seq(nn.Module):
@@ -126,11 +137,14 @@ class Seq2Seq(nn.Module):
         self.decoder = decoder
         self.device = device
         
-        assert encoder.hid_dim == decoder.dec_hid_dim, \
-            "Hidden dimensions of encoder and decoder must be equal!"
+        if encoder.bidirectional:
+          assert encoder.hid_dim * 2 == decoder.dec_hid_dim, \
+              "Hidden dimensions of encoder and decoder must be equal!"
+        else:
+          assert encoder.hid_dim == decoder.dec_hid_dim, \
+              "Hidden dimensions of encoder and decoder must be equal!"          
         
     def forward(self, src, trg, teacher_forcing_ratio = 0.5):
-        
         # src = [src sent len, batch size]
         # trg = [trg sent len, batch size]
         # teacher_forcing_ratio is probability to use teacher forcing
@@ -151,16 +165,15 @@ class Seq2Seq(nn.Module):
         input = trg[0,:]
         
         for t in range(1, trg_len):
-
-             '''your code'''
+            output, hidden = self.decoder(input, hidden, enc_states)
 
             outputs[t] = output
             #decide if we are going to use teacher forcing or not
             teacher_force = random.random() < teacher_forcing_ratio
             #get the highest predicted token from our predictions
-            top1 = output.argmax(-1) 
+            top1 = output.argmax(-1)[0]
             #if teacher forcing, use actual next token as next input
             #if not, use predicted token
             input = trg[t] if teacher_force else top1
         
-        return outputs
+        return outputs 
